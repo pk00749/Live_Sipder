817387e70e3e2de9f1b301ea69c964020bd166a0
pk00749 <https://github.com/pk00749/>
2017-12-12 23:10:58 +0800

    douyu

--- douyu.py
+++ douyu.py
@@ -1,4 +1,3 @@
-import os
 import re
 import urllib.request as ur
 import pandas as pd
@@ -9,6 +8,7 @@ class DySpyder():
 
     def __init__(self, url):
         self.url = url
+        self.soup =""
 
     def open_url(self, url):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
@@ -18,35 +18,36 @@ class DySpyder():
 
     def from_url_get_all_lis(self):
         data = self.open_url(self.url)
-        soup1 = BeautifulSoup(data, 'html.parser')
-        soup = soup1.findAll("li")
+        self.soup = BeautifulSoup(data, 'html.parser')
+        soup = self.soup.findAll("li")
         return soup
 
     def tv_spyder(self, x):
         rid = re.findall(""".*?data-rid="(.*?)".*""", str(x))[0]
         title = re.findall(""".*?title=(.*?)>.*""", str(x))[0]
         href = re.findall(""".*?href="(.*?)".*""", str(x))[0]
-        pic = re.findall('''.*?<img data-original="(.*?)".*''', str(x))[0]
+        # pic = re.findall('''.*?<img data-original="(.*?)".*''', str(x))[0]
         tag = re.findall('''.*<span class="tag ellipsis">(.*?)</span>.*''', str(x))[0]
         name = re.findall('''.*<span class="dy-name ellipsis fl">(.*?)</span>.*''', str(x))[0]
-        see_num = re.findall('''.*<span class="dy-num fr".*?>(.*?)</span>.*''', str(x))[0]
-        t = rid, pic, title, tag, name, see_num, href
+        see_num = re.findall(""".*<span class="dy-num fr".*?>(.*?)</span>.*""", str(x))[0]
+        t = rid,  title, tag, name, see_num, href
         return t
 
 def get_url(page):
     return "https://www.douyu.com/directory/all?page="+ str(page) +"&isAjax=1"
 
-res1 = []
-for i in [j+1 for j in range(1)]:
-    douyu = DySpyder(get_url(i))
-    for x in douyu.from_url_get_all_lis():
-        print(x)
-        try:
-            res1.append(list(douyu.tv_spyder(x)))
-        except:
-            print("Fail...")
 
-print(res1)
-df = pd.DataFrame(np.array(res1))
-print(df)
-df.to_csv("demo.csv")
+if __name__ == '__main__':
+    res1 = []
+    for i in [j + 1 for j in range(1)]:
+        douyu = DySpyder(get_url(i))
+
+        print(douyu.from_url_get_all_lis())
+        for x in douyu.from_url_get_all_lis():
+            try:
+                res1.append(list(douyu.tv_spyder(x)))
+            except:
+                print("Fail...")
+
+    df = pd.DataFrame(np.array(res1))
+    df.to_csv("demo.csv")
