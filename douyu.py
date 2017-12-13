912d7b2c72005374e8d0f82374aa1e944cf16e57
pk00749 <https://github.com/pk00749/>
2017-12-14 00:45:54 +0800

    douyu

--- douyu.py
+++ douyu.py
@@ -9,22 +9,18 @@ TOTAL_PAGES = 2
 
 class DySpyder:
 
-    def __init__(self,page):
-        self.url = self.get_url()
-        self.soups = self.from_url_get_all_lis()
-
-    def get_url(self):
+    def get_url(self,page):
         print("https://www.douyu.com/directory/all?page=" + str(page) + "&isAjax=1")
         return "https://www.douyu.com/directory/all?page=" + str(page) + "&isAjax=1"
 
-    def open_url(self):
+    def open_url(self,page):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
-        req = ur.Request(url=self.url, headers=headers)
+        req = ur.Request(url=self.get_url(page), headers=headers)
         response = ur.urlopen(req)
         return response.read().decode('utf-8')
 
-    def from_url_get_all_lis(self):
-        data = self.open_url()
+    def from_url_get_all_lis(self,page):
+        data = self.open_url(page)
         soup = BeautifulSoup(data, 'html.parser').findAll("li")
         return soup
 
@@ -39,18 +35,31 @@ class DySpyder:
         info = rid,  title, tag, name, see_num, href
         return info
 
-    def save(self):
+    def export(self):
         list_anchor = []
-        for soup in self.soups:
-            try:
-                list_anchor.append(list(douyu.tv_spyder(soup)))
-            except:
-                print("Fail...")
+        for page in range(1, TOTAL_PAGES + 1):
+            for soup in self.from_url_get_all_lis(page):
+                try:
+                    list_anchor.append(list(self.tv_spyder(soup)))
+                except:
+                    print("Fail...")
+
         df = pd.DataFrame(np.array(list_anchor))
-        df.to_csv('page_%s.csv' % (page))
+        df.to_csv('demo.csv')
 
 
 if __name__ == '__main__':
-    for page in range(1, TOTAL_PAGES+1):
-        douyu = DySpyder(page)
-        douyu.save()
+    douyu = DySpyder()
+    douyu.export()
+
+
+    # def save(self):
+    #     for soup in self.soups:
+    #         try:
+    #             self.list_anchor.append(list(douyu.tv_spyder(soup)))
+    #         except:
+    #             print("Fail...")
+    #
+    # def export(self):
+    #     self.save()
+    #     return self.list_anchor
