0f074db8337f84d5f8081ff847600f930cd2e80b
pk00749 <https://github.com/pk00749/>
2018-04-15 00:39:57 +0800

    Scrapy

--- huya/spiders/huyaSpider.py
+++ huya/spiders/huyaSpider.py
@@ -15,50 +15,72 @@ from huya.spiders.get_rooms import GetRooms
 
 class HuyaSpider(CrawlSpider):
     name = 'huya'
-    allowed_domains = ["huya.com"]
-    start_urls = ['https://www.huya.com/g/']
-    file_path = 'G:\Program\Projects\Live_Sipder\huya.xlsx'
-    workbook = AdminWorkbook(file_path)
-    get_room = GetRooms(file_path)
     total_rooms = 0
+    no = 2
+    file_path = 'G:\Program\Projects\Live_Sipder\huya.xlsx'
+    get_room = GetRooms(file_path)
+    workbook = AdminWorkbook(file_path)
+    allowed_domains = ["huya.com"]
 
-    # def start_requests(self):
-    #     url = self.get_room.get_topic_url(1+1)
-    #     print('url: '+url)
-    #     yield Request('https://www.huya.com/g/xingxiu', callback=self.parse)
+    start_urls = ['https://www.huya.com/g/']
+
+    def start_requests(self):
+        # url = self.get_room.get_topic_url(1+1)
+        for url in self.start_urls:
+            print('url: '+url)
+            yield Request(url, callback=self.parse)
 
 
     def parse(self, response):
-    #     self.workbook.load_workbook()
-    #     live_rooms = response.xpath('//ul[@class="live-list clearfix"]/li/a[@class="title new-clickstat"]/@herf')
-        # live_rooms = page_soup.find("ul", attrs={"class": "live-list clearfix"})
+        self.logger.info("Visited %s", response.url)
+        movies = response.xpath('//ul[@class="game-list clearfix"]/li')
+        topic = self.workbook.read_cell('登录', 'C%d' % self.no)
+        print('topic: ' + topic)
+        self.workbook.create_sheet(topic)
+        for each_movie in movies:
+            if topic == each_movie.xpath('./a/img/@title').extract()[0]:
+                href = each_movie.xpath('./a/@href').extract()[0]
+                print('href: ' + href)
+                yield Request(href, callback=self.get_topic_url)
+            else:
+                continue
+
+    def get_topic_url(self, response):
+        self.logger.info("Visited %s", response.url)
+        live_rooms = response.xpath(
+            '//ul[@class="live-list clearfix"]/li/a[@class="title new-clickstat"]/@href').extract()
         # total_rooms_in_page = len(live_rooms.find_all('a', class_='title new-clickstat', href=True))
         #
         # current_total_rooms_in_topic = self.total_rooms + total_rooms_in_page
         # print(live_rooms)
-        # for room in live_rooms:
-        #     item = HuyaItem()
-        #     item['room'] = room # .xpath('.//a[@class="title new-clickstat"]/@herf')
-        #     print(item['room'])
-        # r = 1
-        # for a in live_rooms.find_all('a', class_='title new-clickstat', href=True):
-        #     print(a['href'], a['title'])
-        #     if self.total_rooms + r <= current_total_rooms_in_topic:
-        #         print(self.total_rooms + r)
-        #         self.workbook.write_cell(self.topic, 'A%d' % (self.total_rooms + r), a['title'])
-        #         self.workbook.write_cell(self.topic, 'B%d' % (self.total_rooms + r), a['href'])
-        #         r += 1
-
-
-        movies = response.xpath('//ul[@class="game-list clearfix"]/li')
-        i = 1
-        for each_movie in movies:
+        for room in live_rooms:
             item = HuyaItem()
-            item['topic'] = each_movie.xpath('./a/img/@title').extract()[0]
-            item['href'] = each_movie.xpath('./a/@href').extract()[0]
-            print(item['topic'])
-            self.workbook.write_cell('主题列表', 'A%d' % i, item['topic'])
-            self.workbook.write_cell('主题列表', 'B%d' % i, item['href'])
-            i = i + 1
-            yield item
-            self.workbook.save_workbook()
+            item['room'] = room  # .xpath('.//a[@class="title new-clickstat"]/@herf')
+            print(item['room'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # self.workbook.load_workbook()
+        # movies = response.xpath('//ul[@class="game-list clearfix"]/li')
+        # i = 1
+        # for each_movie in movies:
+        #     item = HuyaItem()
+        #     item['topic'] = each_movie.xpath('./a/img/@title').extract()[0]
+        #     item['href'] = each_movie.xpath('./a/@href').extract()[0]
+        #     print(item['topic'])
+        #     self.workbook.write_cell('主题列表', 'A%d' % i, item['topic'])
+        #     self.workbook.write_cell('主题列表', 'B%d' % i, item['href'])
+        #     i = i + 1
+        #     yield item
+        #     self.workbook.save_workbook()
