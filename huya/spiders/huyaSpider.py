26266701f9f65708f2dcea900d97e469afbe4a0e
pk00749 <https://github.com/pk00749/>
2018-04-14 18:08:36 +0800

    Scrapy

--- huya/spiders/huyaSpider.py
+++ huya/spiders/huyaSpider.py
@@ -16,30 +16,30 @@ from huya.spiders.get_rooms import GetRooms
 class HuyaSpider(CrawlSpider):
     name = 'huya'
     allowed_domains = ["huya.com"]
-    start_urls = ['https://www.huya.com/g/xingxiu']
+    start_urls = ['https://www.huya.com/g/']
     file_path = 'G:\Program\Projects\Live_Sipder\huya.xlsx'
     workbook = AdminWorkbook(file_path)
     get_room = GetRooms(file_path)
     total_rooms = 0
 
-    def start_requests(self):
-        url = self.get_room.get_topic_url(1+1)
-        print('url: '+url)
-        yield Request('https://www.huya.com/g/xingxiu', callback=self.parse)
+    # def start_requests(self):
+    #     url = self.get_room.get_topic_url(1+1)
+    #     print('url: '+url)
+    #     yield Request('https://www.huya.com/g/xingxiu', callback=self.parse)
 
 
     def parse(self, response):
-        self.workbook.load_workbook()
-        live_rooms = response.xpath('//ul[@class="live-list clearfix"]/li//a[@class="title new-clickstat"]/@herf')
+    #     self.workbook.load_workbook()
+    #     live_rooms = response.xpath('//ul[@class="live-list clearfix"]/li/a[@class="title new-clickstat"]/@herf')
         # live_rooms = page_soup.find("ul", attrs={"class": "live-list clearfix"})
         # total_rooms_in_page = len(live_rooms.find_all('a', class_='title new-clickstat', href=True))
         #
         # current_total_rooms_in_topic = self.total_rooms + total_rooms_in_page
-        print(live_rooms)
-        for room in live_rooms:
-            item = HuyaItem()
-            item['room'] = room # .xpath('.//a[@class="title new-clickstat"]/@herf')
-            print(item['room'])
+        # print(live_rooms)
+        # for room in live_rooms:
+        #     item = HuyaItem()
+        #     item['room'] = room # .xpath('.//a[@class="title new-clickstat"]/@herf')
+        #     print(item['room'])
         # r = 1
         # for a in live_rooms.find_all('a', class_='title new-clickstat', href=True):
         #     print(a['href'], a['title'])
@@ -50,15 +50,15 @@ class HuyaSpider(CrawlSpider):
         #         r += 1
 
 
-        # movies = response.xpath('//ul[@class="game-list clearfix"]/li')
-        # i = 1
-        # for each_movie in movies:
-        #     item = HuyaItem()
-        #     item['topic'] = each_movie.xpath('./a/img/@title').extract()[0]
-        #     item['href'] = each_movie.xpath('./a/@href').extract()[0]
-        #     print(item['topic'])
-        #     self.workbook.write_cell('主题列表', 'A%d' % i, item['topic'])
-        #     self.workbook.write_cell('主题列表', 'B%d' % i, item['href'])
-        #     i = i + 1
-        #     yield item
-        #     self.workbook.save_workbook()
+        movies = response.xpath('//ul[@class="game-list clearfix"]/li')
+        i = 1
+        for each_movie in movies:
+            item = HuyaItem()
+            item['topic'] = each_movie.xpath('./a/img/@title').extract()[0]
+            item['href'] = each_movie.xpath('./a/@href').extract()[0]
+            print(item['topic'])
+            self.workbook.write_cell('主题列表', 'A%d' % i, item['topic'])
+            self.workbook.write_cell('主题列表', 'B%d' % i, item['href'])
+            i = i + 1
+            yield item
+            self.workbook.save_workbook()
